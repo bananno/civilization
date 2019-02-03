f9e6127bb4e9698478bd6efeff8023dbf3fd7982
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-02 20:08:21 -0800

    Continue building routes for loading games

--- routes/games.js
+++ routes/games.js
@@ -2,24 +2,37 @@ const express = require('express');
 const router = express.Router();
 const Game = require('../models/game');
 
-router.get('/chooseGame', getChooseGame);
-router.post('/loadGame', postLoadGame);
-router.post('/newGame', postNewGame);
+router.get('/loadGame', loadGameGet);
+router.post('/loadGame', loadGamePost);
+router.get('/newGame', newGameGet);
+router.post('/newGame', newGamePost);
 
-function getChooseGame(req, res, next) {
-  res.render('chooseGame', { });
+function loadGameGet(req, res, next) {
+  Game.find({ }, function (error, games) {
+    if (error) {
+      res.send({ message: 'ERROR: ' + error });
+    } else {
+      res.render('loadGame', {
+        games: games
+      });
+    }
+  });
 }
 
-function postLoadGame(req, res, next) {
+function loadGamePost(req, res, next) {
 
 }
 
-function postNewGame(req, res, next) {
+function newGameGet(req, res, next) {
+  res.render('newGame', { });
+}
+
+function newGamePost(req, res, next) {
   var gameData = {
     name: req.body.name || '',
   };
 
-  if (gameData.name.length) {
+  if (gameData.name.length === 0) {
     return res.send({ message: 'All fields are required.' });
   }
 
