9d18f20e53cda6faa85cbe9ff9f07e51046c19b7
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-02 19:25:14 -0800

    Create main game model

---
+++ models/game.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var GameSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
+
+GameSchema.statics.authenticate = (username, callback) => {
+  Game.findOne({ username: username }, (error, game) => {
+    if (error) {
+      return callback(error)
+    } else if (!user) {
+      let newError = new Error('Game not found.');
+      newError.status = 401;
+      return callback(newError);
+    }
+  });
+};
+
+var Game = mongoose.model('Game', GameSchema);
+module.exports = Game;
