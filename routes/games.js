ec83b75f15f47f5c1acfc26431ef35dbd0afa042
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-02 19:36:28 -0800

    Add a route for creating new game

--- routes/games.js
+++ routes/games.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Game = require('../models/game');
+
+router.post('/newgame', newgame);
+
+function newgame(req, res, next) {
+  var gameData = {
+    name: req.body.name || '',
+  };
+
+  if (gameData.name.length) {
+    return res.send({ message: 'All fields are required.' });
+  }
+
+  Game.create(gameData, function (error, game) {
+    if (error) {
+      return res.send({ message: 'ERROR: ' + error });
+    } else {
+      req.session.gameId = game._id;
+      return res.send({ game: game });
+    }
+  });
+}
 
 module.exports = router;
