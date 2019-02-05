84e76c1478760b388912309478c51c1d5d390c3d
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-05 14:14:02 -0800

    Hide rows that are not within a few tiles

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -25,4 +25,20 @@ function setActiveUnit(id) {
 
   $('.active-unit').hide();
   $('.active-unit[unit-id="' + id + '"]').show();
+
+  centerMap();
+}
+
+function centerMap() {
+  $('.map-row').hide();
+
+  let startRow = mapCenter[0] - 5;
+  if (startRow < 0) {
+    startRow = 0;
+  }
+  let endRow = startRow + 10;
+
+  for (let r = startRow; r <= endRow; r++) {
+    $('.map-row').filter('[row="' + r + '"]').show();
+  }
 }
