291a468125e165b09e6ea926bf934ef4d22e9de8
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-04 19:55:44 -0800

    Organize front-end JS

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,4 +1,17 @@
 
+function setupActiveUnitSelection() {
+  $('.unit.turn').toArray().forEach(unit => {
+    let $unit = $(unit);
+    $unit.click(() => {
+      setActiveUnit($unit.attr('unit-id'));
+    });
+  });
+
+  $('.active-unit .close').click(() => {
+    setActiveUnit(null);
+  });
+}
+
 function showPlayersList(show) {
   $('#players-box').toggle(show);
 }
@@ -6,9 +19,10 @@ function showPlayersList(show) {
 function setActiveUnit(id) {
   $('.unit').removeClass('active');
   $('.unit[unit-id="' + id + '"]').addClass('active');
+
   $('form.move-unit').hide();
   $('form.move-unit[unit-id="' + id + '"]').show();
 
-  $('.infobox').hide();
-  $('.infobox[unit-id="' + id + '"]').show();
+  $('.active-unit').hide();
+  $('.active-unit[unit-id="' + id + '"]').show();
 }
