bdafae8059fc57b463a489a16069885de2101663
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-04 16:35:36 -0800

    Move function to file

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,3 +2,13 @@
 function showPlayersList(show) {
   $('#players-box').toggle(show);
 }
+
+function setActiveUnit(id) {
+  $('.unit').removeClass('active');
+  $('.unit[unit-id="' + id + '"]').addClass('active');
+  $('form.move-unit').hide();
+  $('form.move-unit[unit-id="' + id + '"]').show();
+
+  $('.infobox').hide();
+  $('.infobox[unit-id="' + id + '"]').show();
+}
