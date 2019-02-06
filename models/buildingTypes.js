5349ce7d3b1b5f6d6591cc4ac9996ac8b939b659
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-06 10:58:35 -0800

    Add production for building types; show income in city info box

--- models/buildingTypes.js
+++ models/buildingTypes.js
@@ -4,11 +4,13 @@ const buildingTypes = [
     name: 'palace',
     cost: 0,
     gold: 5,
+    production: 5,
   },
   {
     name: 'granary',
     cost: 60,
     gold: -1,
+    production: 0,
   }
 ];
 
