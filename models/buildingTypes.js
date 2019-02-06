aeb8475c1de2ee7143e5ae0f6255cadeca5d2416
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-06 10:14:27 -0800

    Add granary; add construction costs for building types

--- models/buildingTypes.js
+++ models/buildingTypes.js
@@ -2,8 +2,14 @@
 const buildingTypes = [
   {
     name: 'palace',
+    cost: 0,
     gold: 5,
   },
+  {
+    name: 'granary',
+    cost: 60,
+    gold: -1,
+  }
 ];
 
 module.exports = buildingTypes;
