549aff554810a66d76f6f3ea6f62559e0b223ba3
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-06 00:03:37 -0800

    Add worker and production costs for unit types

--- models/unitTypes.js
+++ models/unitTypes.js
@@ -3,10 +3,17 @@ const unitTypes = [
   {
     name: 'settler',
     moves: 2,
+    cost: 106,
   },
   {
     name: 'scout',
     moves: 2,
+    cost: 25,
+  },
+  {
+    name: 'settler',
+    moves: 2,
+    cost: 70,
   },
 ];
 
