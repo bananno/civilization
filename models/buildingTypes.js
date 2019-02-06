b02de646cbb1dd460e5dc9499415938ef16c7fc9
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-06 14:15:47 -0800

    Add food attribute for buildings and show city's food production

--- models/buildingTypes.js
+++ models/buildingTypes.js
@@ -4,12 +4,14 @@ const buildingTypes = [
     name: 'palace',
     cost: 0,
     gold: 5,
+    food: 2,
     production: 5,
   },
   {
     name: 'granary',
     cost: 60,
     gold: -1,
+    food: 2,
     production: 0,
   }
 ];
