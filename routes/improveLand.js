0863ea6b7c34720831d6b89f6e9ddf4407618e98
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-06 15:58:27 -0800

    Only allow worker improvements when land is owned by current player

--- routes/improveLand.js
+++ routes/improveLand.js
@@ -14,9 +14,14 @@ router.post('/improveLand/:unitId/:improvement', (req, res, next) => {
       return unit._id == unitId;
     })[0];
 
+    let tile = data.tiles.filter(tile => {
+      return tile.row == unit.location[0] && tile.column == unit.location[1];
+    })[0];
+
     let turnPlayerId = data.players[data.game.nextPlayer]._id;
 
     if (unit.unitType.name != 'worker' || unit.movesRemaining == 0
+        || '' + tile.owner != '' + turnPlayerId
         || '' + unit.player != '' + turnPlayerId) {
       console.log('invalid unit action');
       return res.redirect('/');
