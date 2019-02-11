436bcbca2a2413f7ad6344d70bb4dfb57ba91a0b
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-11 07:01:50 -0800

    Add comment outlining rules for tile visibility

--- routes/getVisibleTiles.js
+++ routes/getVisibleTiles.js
@@ -1,4 +1,39 @@
 
+/*
+
+VISIBILITY RULES
+
+A unit can see up to 2 tiles away. For the purpose of visibility, diagonal tiles are considered
+  adjacent (unlike movement, in which diagonal moves are a sum of 2 adjacent moves.) So the
+  maximum visibility range is a 5x5 tile area.
+
+All adjacent tiles are always visible, so the minimum visibility range is a 3x3 tile range.
+
+Adjacent forests, hills, and mountains block visibility. Higher objects can be seen beyond lower
+objects. The unit can see further when on a hill, and not as far when in a forest.
+
+| | | | | |
+| | | | | |
+| | |1| | |
+| | |2|3| |
+| | |4|5|6|
+
+Diagram:
+  The unit is locationed at (1).
+  The far corner (6) is visible if the adjacent corner (3) is see-through.
+  The far edge (4) is visible if the adjacent edge (2) is see-through.
+  The far in-between (5) is visible if either of the adjacent tiles (2 or 3) is see-through.
+
+Terrain:
+  Adjacent tiles are always visible.
+  When the unit is in a forest, no far tiles are visible.
+  When the adjacent tile is a mountain, the far tile is not visible.
+  When the adjacent tile is a hill, the far tile is visible if it is a mountain.
+  When the adjacent tile is a (flat) forest, the far tile is visible if the unit is on a hill, OR
+    if the far tile is a hill or mountain.
+
+*/
+
 const immediateCorners = [[-1, -1], [-1, 1], [1, -1], [1, 1]];
 const immediateEdges = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
