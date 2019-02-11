4f8e6b5cc08886696d659221493b3636229014e9
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-10 16:08:28 -0800

    Continue building unit visibility function

--- routes/getVisibleTiles.js
+++ routes/getVisibleTiles.js
@@ -5,29 +5,53 @@ const getVisibleTilesFunction = (data) => {
   return (row, column) => {
     let coords = [];
 
-    for (let r = row - 2; r <= row + 2; r++) {
-      if (r < 0) {
-        continue;
-      }
-      if (r >= numRows) {
-        break;
-      }
-      for (let c1 = column - 2; c1 <= column + 2; c1++) {
-        let c = c1;
-        if (c < 0) {
-          c += numCols;
-        } else if (c >= numCols) {
-          c -= numCols;
-        }
+    const isFlat = (r1, c1) => {
+      let r = row + r1;
+      let c = column + c1;
 
-        coords.push([r, c]);
+      return true;
+    };
+
+    const saveTile = (r1, c1) => {
+      let r = row + r1;
+      let c = column + c1;
+
+      if (r < 0 || r >= numRows) {
+        return;
       }
+
+      if (c < 0) {
+        c += numCols;
+      } else if (c >= numCols) {
+        c -= numCols;
+      }
+
+      coords.push([r, c]);
+    };
+
+    for (let r1 = -1; r1 <= 1; r1++) {
+      for (let c1 = -1; c1 <= 1; c1++) {
+        saveTile(r1, c1);
+      }
+    }
+
+    if (isFlat(-1, 0)) {
+      saveTile(-2, 0);
+    }
+    if (isFlat(0, -1)) {
+      saveTile(0, -2);
+    }
+    if (isFlat(1, 0)) {
+      saveTile(2, 0);
+    }
+    if (isFlat(0, 1)) {
+      saveTile(0, 2);
     }
 
     return coords;
   };
 
   return getVisibleTiles;
-}
+};
 
 module.exports = getVisibleTilesFunction;
