3b39f15f973785fe53df4008e26ebd2e74a5bc57
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-18 11:48:57 -0800

    Add land patterns

--- routes/createMap.js
+++ routes/createMap.js
@@ -1,11 +1,23 @@
 const mapPatterns = require('../models/mapPatterns');
+const helpers = require('./helpers');
 
-const createMap = (game) => {
-  const [numRows, numCols] = game.mapSize;
+const map = [];
+const tiles = [];
+let numRows, numCols;
 
-  const map = [];
-  const tiles = [];
+function createMap(game) {
+  [numRows, numCols] = game.mapSize;
 
+  createBasicMap(game);
+
+  for (let count = 0; count < 10; count++) {
+    addLandPattern();
+  }
+
+  return tiles;
+}
+
+function createBasicMap(game) {
   for (let r = 0; r < numRows; r++) {
     map[r] = [];
     for (let c = 0; c < numCols; c++) {
@@ -33,8 +45,36 @@ const createMap = (game) => {
       tiles.push(tileData);
     }
   }
+}
 
-  return tiles;
-};
+function addLandPattern() {
+  const patternNumber = helpers.getRandomInt(0, mapPatterns.land.length - 1);
+  const pattern = mapPatterns.land[patternNumber];
+  const startRangeRow = 0 - pattern.length;
+  const endRangeRow = numRows + pattern.length;
+
+  const startRow = helpers.getRandomInt(startRangeRow, endRangeRow);
+  const startCol = helpers.getRandomInt(0, numCols);
+
+  for (let r1 = 0; r1 < pattern.length; r1++) {
+    let r = startRow + r1;
+
+    if (r < 0) {
+      continue;
+    }
+
+    if (r >= numRows) {
+      break;
+    }
+
+    for (let c1 = 0; c1 < pattern[0].length; c1++) {
+      if (pattern[r1][c1] == 1) {
+        let c = helpers.getColumn(numCols, startCol + c1);
+        map[r][c].terrain.ground = 'grassland';
+        map[r][c].terrain.water = false;
+      }
+    }
+  }
+}
 
 module.exports = createMap;
