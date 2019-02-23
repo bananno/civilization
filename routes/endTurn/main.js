4b07c8476ef021c420d537ea2e46fd3b7fe1d0ab
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-23 14:48:46 -0800

    Fix bug and refactor

--- routes/endTurn/main.js
+++ routes/endTurn/main.js
@@ -8,35 +8,30 @@ const updateCities = require('./updateCities');
 const updatePlayer = require('./updatePlayer');
 
 router.post('/endTurn', (req, res, next) => {
-  const done = () => {
-    res.redirect('/');
-  };
-
   getData(req, res, next, (data) => {
-    endTurn(data, done);
+    if (!allCitiesHaveProject(data)) {
+      console.log('All cities must have a project to end turn.');
+      return res.redirect('/');
+    }
+
+    if (playerNeedsResearch(data)) {
+      console.log('Player must choose research before ending turn.');
+      return res.redirect('/');
+    }
+
+    endTurn(data);
+    res.redirect('/');
   });
 });
 
-async function endTurn(data, done) {
-  if (!allCitiesHaveProject(data.currentPlayer, data.cities)) {
-    console.log('All cities must have a project to end turn.');
-    return done();
-  }
-
-  if (playerNeedsResearch(data.currentPlayer, data.technologyList)) {
-    console.log('Player must choose research before ending turn.');
-    return done();
-  }
-
+async function endTurn(data) {
   await updateGame(data);
   await updateUnits(data);
   await updateCities(data);
   await updatePlayer(data);
-
-  done();
 }
 
-function updateGame(data) {
+async function updateGame(data) {
   const gameData = {};
 
   if (data.game.nextPlayer < data.players.length - 1) {
@@ -46,26 +41,28 @@ function updateGame(data) {
     gameData.turn = data.game.turn + 1;
   }
 
-  data.game.update(gameData);
+  await data.game.update(gameData);
 }
 
-function allCitiesHaveProject(player, cities) {
-  for (let i = 0; i < cities.length; i++) {
-    if ('' + cities[i].player != '' + player._id) {
+function allCitiesHaveProject(data) {
+  for (let i = 0; i < data.cities.length; i++) {
+    const city = data.cities[i];
+
+    if (!data.help.isCurrentPlayer(city)) {
       continue;
     }
-    if (cities[i].project == null || cities[i].project.category == null
-        || cities[i].project.category == '') {
+
+    if (city.project == null || city.project.category == null || city.project.category == '') {
       return false;
     }
   }
   return true;
 }
 
-function playerNeedsResearch(player, technologyList) {
-  return player.researchCurrent == null
-    && player.production.science > 0
-    && technologyList.filter(tech => tech.isAvailable).length > 0;
+function playerNeedsResearch(data) {
+  return data.currentPlayer.researchCurrent == null
+    && data.currentPlayer.production.science > 0
+    && data.technologyList.filter(tech => tech.isAvailable).length > 0;
 }
 
 module.exports = router;
