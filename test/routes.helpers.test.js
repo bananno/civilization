1994a7def6b8dc2a736265f72bdf33fd0c845312
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-26 22:04:54 -0800

    Add tests for adjacent tile direction function

--- test/routes.helpers.test.js
+++ test/routes.helpers.test.js
@@ -11,7 +11,48 @@ const data = {
 const help = getHelpers.makeHelperFunctions(data);
 
 describe('Helpers', () => {
-  describe('#getColumn(colNumber)', () => {
+  describe('getAdjacentDirection(fromRow, fromCol, toRow, toCol)', () => {
+    it('directly left or right when in middle of map', () => {
+      assert.equal(help.getAdjacentDirection(10, 10, 10, 9), 'left');
+      assert.equal(help.getAdjacentDirection(10, 10, 10, 11), 'right');
+    });
+    it('directly left or right overlapping with map edge', () => {
+      assert.equal(help.getAdjacentDirection(10, 0, 10, 39), 'left');
+      assert.equal(help.getAdjacentDirection(10, 39, 10, 0), 'right');
+    });
+    it('up and down, starting in an even row, in middle of map', () => {
+      assert.equal(help.getAdjacentDirection(10, 10, 9, 10), 'up-left');
+      assert.equal(help.getAdjacentDirection(10, 10, 9, 11), 'up-right');
+      assert.equal(help.getAdjacentDirection(10, 10, 11, 10), 'down-left');
+      assert.equal(help.getAdjacentDirection(10, 10, 11, 11), 'down-right');
+    });
+    it('up and down, starting in an even row, overlapping with map edge', () => {
+      assert.equal(help.getAdjacentDirection(10, 0, 9, 0), 'up-left');
+      assert.equal(help.getAdjacentDirection(10, 39, 9, 0), 'up-right');
+      assert.equal(help.getAdjacentDirection(10, 0, 11, 0), 'down-left');
+      assert.equal(help.getAdjacentDirection(10, 39, 11, 0), 'down-right');
+    });
+    it('up and down, starting in an odd row, in middle of map', () => {
+      assert.equal(help.getAdjacentDirection(11, 10, 10, 9), 'up-left');
+      assert.equal(help.getAdjacentDirection(11, 10, 10, 10), 'up-right');
+      assert.equal(help.getAdjacentDirection(11, 10, 12, 9), 'down-left');
+      assert.equal(help.getAdjacentDirection(11, 10, 12, 10), 'down-right');
+    });
+    it('up and down, starting in an odd row, overlapping with map edge', () => {
+      assert.equal(help.getAdjacentDirection(11, 0, 10, 39), 'up-left');
+      assert.equal(help.getAdjacentDirection(11, 39, 10, 39), 'up-right');
+      assert.equal(help.getAdjacentDirection(11, 0, 12, 39), 'down-left');
+      assert.equal(help.getAdjacentDirection(11, 39, 12, 39), 'down-right');
+    });
+    it('should work the same way whether one, both, or neither pair is an array', () => {
+      assert.equal(help.getAdjacentDirection(10, 10, 10, 9), 'left');
+      assert.equal(help.getAdjacentDirection([10, 10], [10, 9]), 'left');
+      assert.equal(help.getAdjacentDirection([10, 10], 10, 9), 'left');
+      assert.equal(help.getAdjacentDirection(10, 10, [10, 9]), 'left');
+    });
+  });
+
+  describe('getColumn(colNumber)', () => {
     it('should return given number if it is between 0 and the number of columns', () => {
       assert.equal(help.getColumn(0), 0);
       assert.equal(help.getColumn(10), 10);
@@ -27,7 +68,7 @@ describe('Helpers', () => {
     });
   });
 
-  describe('#getTileTotalProduction(tile)', () => {
+  describe('getTileTotalProduction(tile)', () => {
     it('should return the sum of all production types for given tile', () => {
       const tile = {
         production: {
@@ -38,7 +79,7 @@ describe('Helpers', () => {
     });
   });
 
-  describe('#sameLocation(pair1, pair2)', () => {
+  describe('sameLocation(pair1, pair2)', () => {
     it('should return true when the two pairs are equal', () => {
       assert.equal(help.sameLocation([17, 9], [17, 9]), true);
     });
@@ -47,7 +88,7 @@ describe('Helpers', () => {
     });
   });
 
-  describe('#isSamePlayer(player1, player2)', () => {
+  describe('isSamePlayer(player1, player2)', () => {
     it('should return true when both values are null', () => {
       assert.equal(help.isSamePlayer(null, null), true);
     });
