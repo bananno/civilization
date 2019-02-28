56c87f88edf1d22f1498879fe901ab8433002504
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-28 10:35:09 -0800

    Fix indentation; add helper function

--- routes/newGame.js
+++ routes/newGame.js
@@ -14,90 +14,90 @@ router.post('/newGame', newGame);
 async function newGame(req, res, next) {
   const game = await createGame(req.body);
 
-      req.session.gameId = game._id;
+  req.session.gameId = game._id;
 
-      const numPlayers = 2;
+  const numPlayers = 2;
 
-      let tileList = createMap(game);
+  let tileList = createMap(game);
 
-      const unitLocations = chooseUnitLocations(tileList, numPlayers);
+  const unitLocations = chooseUnitLocations(tileList, numPlayers);
 
-      const getVisibleTiles = getVisibleTilesFunction({
-        game: { mapSize: game.mapSize },
-        tiles: tileList,
-      });
+  const getVisibleTiles = getVisibleTilesFunction({
+    game: { mapSize: game.mapSize },
+    tiles: tileList,
+  });
 
-      const createPlayer = (i) => {
-        var playerData = {
-          game: game,
-          name: req.body['playername_' + i].trim() || 'Player ' + (i + 1),
-        };
+  const createPlayer = (i) => {
+    var playerData = {
+      game: game,
+      name: req.body['playername_' + i].trim() || 'Player ' + (i + 1),
+    };
 
-        Player.create(playerData, (error, player) => {
-          if (error) {
-            return next(error);
-          }
-
-          var tempUnit1 = {
-            game: game,
-            player: player,
-            location: unitLocations[i][0],
-            templateName: 'settler',
-          };
-
-          var tempUnit2 = {
-            game: game,
-            player: player,
-            location: unitLocations[i][1],
-            templateName: 'scout',
-          };
-
-          let revealedTiles = [];
-
-          revealedTiles = revealedTiles.concat(getVisibleTiles(tempUnit1.location));
-          revealedTiles = revealedTiles.concat(getVisibleTiles(tempUnit2.location));
-
-          revealedTiles.forEach(pair => {
-            tileList = tileList.map(tile => {
-              if (helpers.sameLocation(pair, tile.location)) {
-                tile.discovered.push(player);
-              }
-              return tile;
-            });
-          });
-
-          createUnit(tempUnit1, () => {
-            createUnit(tempUnit2, () => {
-              if (i < numPlayers - 1) {
-                createPlayer(i + 1);
-              } else {
-                createTile(0);
-              }
-            });
-          });
-        });
-      };
-
-      const createTile = (i) => {
-        if (i >= tileList.length) {
-          res.redirect('/');
-        } else {
-          Tile.create(tileList[i], (error, tile) => {
-            if (error) {
-              return next(error);
-            }
-            createTile(i + 1);
-          });
-        }
+    Player.create(playerData, (error, player) => {
+      if (error) {
+        return next(error);
       }
 
-      createPlayer(0);
+      var tempUnit1 = {
+        game: game,
+        player: player,
+        location: unitLocations[i][0],
+        templateName: 'settler',
+      };
+
+      var tempUnit2 = {
+        game: game,
+        player: player,
+        location: unitLocations[i][1],
+        templateName: 'scout',
+      };
+
+      let revealedTiles = [];
+
+      revealedTiles = revealedTiles.concat(getVisibleTiles(tempUnit1.location));
+      revealedTiles = revealedTiles.concat(getVisibleTiles(tempUnit2.location));
+
+      revealedTiles.forEach(pair => {
+        tileList = tileList.map(tile => {
+          if (helpers.sameLocation(pair, tile.location)) {
+            tile.discovered.push(player);
+          }
+          return tile;
+        });
+      });
+
+      createUnit(tempUnit1, () => {
+        createUnit(tempUnit2, () => {
+          if (i < numPlayers - 1) {
+            createPlayer(i + 1);
+          } else {
+            createTile(0);
+          }
+        });
+      });
+    });
+  };
+
+  const createTile = (i) => {
+    if (i >= tileList.length) {
+      res.redirect('/');
+    } else {
+      Tile.create(tileList[i], (error, tile) => {
+        if (error) {
+          return next(error);
+        }
+        createTile(i + 1);
+      });
+    }
+  }
+
+  createPlayer(0);
 }
 
 async function createGame(params) {
   let numRows = parseInt(params.rows || 0);
   let numCols = parseInt(params.columns || 0);
-  let gameName = (params.name || '').trim() || ('Game' + ('' + Math.random()).slice(2, 10));
+  let gameName = getGameName(params.name);
 
   if (numRows < 10) {
     numRows = 10;
@@ -121,6 +121,18 @@ async function createGame(params) {
       resolve(game);
     });
   });
+}
+
+function getGameName(inputName) {
+  inputName = (inputName || '').trim();
+
+  if (inputName.length) {
+    return inputName;
+  }
+
+  const randomNumber = ('' + Math.random()).slice(2, 10);
+
+  return 'Game' + randomNumber;
 }
 
 function chooseUnitLocations(tiles, numPlayers) {
